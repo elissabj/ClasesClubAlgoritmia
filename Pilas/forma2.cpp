7042b9bd23ce03b0ac5c020d0e6e7acdf66da206
elissabj <56558029+elissabj@users.noreply.github.com>
2022-02-03 10:58:51 -0600

    validarParentesisVersion2

---
+++ Pilas/forma2.cpp
@@ -0,0 +1,85 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct miNodo{
+    char c;
+    miNodo *sig;
+};
+
+struct miPila{
+    miNodo * tope = nullptr;
+    int tam = 0;
+
+    bool empty(){
+        if(tam == 0) return true;
+        return false;
+    }
+
+    int size(){
+        return tam;
+    }
+
+    char top(){
+        return tope->c;
+    }
+
+    void push (char a){
+        tam++;
+        miNodo *nuevoNodo = new miNodo();
+        nuevoNodo->c = a;
+
+        if(tope == nullptr){
+            tope = nuevoNodo;
+        }else{
+            miNodo *nodoDesplazar = tope;
+            tope = nuevoNodo;
+            tope->sig = nodoDesplazar;
+        }
+        return;
+    }
+
+    void pop(){
+        if(tope == nullptr) return;
+        tam--;
+        miNodo *nodoBye = tope;
+        tope = tope->sig;
+        delete nodoBye;
+
+        return;
+    }
+};
+
+bool isCorrect(string &s){
+
+   miPila check;
+   
+   for(int i = 0; i < s.size(); i++){
+       if (s[i] == '(') check.push(')');
+       else if (s[i] == '{') check.push('}');
+       else if (s[i] == '[') check.push(']');
+       else{
+           if(s[i] != check.top() || check.empty()) return false;
+           
+           check.pop();
+       }
+   }
+   
+   return check.empty();
+}
+
+
+int main(){
+
+    int t; cin >> t;
+
+    while(t--){
+        string s; cin >> s; 
+        if(isCorrect(s)){
+            cout <<"YES\n";
+        }else{
+            cout <<"NO\n";
+        }
+    }
+
+return 0;
+}
